68026d67c9ba7062e95630de80226ff6eff8e05c
Rizky Syahri Ramadhan <ramadhancai@gmail.com>
2023-09-03 13:43:39 +0700

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import {redirect} from "next/navigation"
 export default function Home() {
   redirect("/links")
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 ">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 scale">
       
     </main>
   )
